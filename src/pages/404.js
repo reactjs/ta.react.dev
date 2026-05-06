81a6ef9399c71619c20e2fbbeb17e14947b6ae27
Sanjaiyan Parthipan <parthipankalayini@gmail.com>
2026-05-06 19:22:23 +0000

    fix: Improve formatting in 404 and 500 error pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -17,14 +17,17 @@ const {Intro, MaxWidth, p: P, a: A} = MDXComponents;
 
 export default function NotFound() {
   return (
-    <Page toc={[]} meta={{title: 'வலைப்பக்கம் கண்டறியப்படவில்லை'}} routeTree={sidebarLearn}>
+    <Page
+      toc={[]}
+      meta={{title: 'வலைப்பக்கம் கண்டறியப்படவில்லை'}}
+      routeTree={sidebarLearn}>
       <MaxWidth>
         <Intro>
           <P>இந்தப் வலைப்பக்கம் கண்டறியப்படவில்லை.</P>
           <P>
             இதில் ஏதேனும் தவறு இருந்தால் {', '}
             <A href="https://github.com/reactjs/ta.react.dev/issues/new">
-            எங்களுக்குத் தெரியப்படுத்துங்கள் 
+              எங்களுக்குத் தெரியப்படுத்துங்கள்
             </A>
             {', '}
             நாங்கள் அதைச் சரிசெய்ய முயற்சிப்போம் !
